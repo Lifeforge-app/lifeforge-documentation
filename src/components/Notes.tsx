feba5a0e5acdd807f306cf6b56f2372f8c3205e3
Melvin Chia <melvinchia623600@gmail.com>
2025-07-20 08:27:57 +0800

    25w29

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,9 +1,17 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import React from "react";
 
-function Notes({ children }: { children: React.ReactNode }) {
+function Notes({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
   return (
-    <div className="quote w-full p-4 bg-bg-800/70 mt-6 rounded-md border-l-4 border-primary">
+    <div
+      className={`quote w-full p-4 bg-bg-800/70 mt-6 rounded-md border-l-4 border-primary ${className}`}
+    >
       <div className="flex items-center gap-2">
         <Icon icon="tabler:info-circle" className="w-6 h-6 text-primary" />
         <h3 className="text-xl font-semibold">Notes</h3>
